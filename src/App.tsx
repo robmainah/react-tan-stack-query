1edaef3b7a42c697cee23fbee526b88f801de03e
robmainah <44176348+robmainah@users.noreply.github.com>
2023-11-16 21:52:37 +0300

    fetch posts and create mutations on creating new post

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { QueryClient, useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
+
+const POSTS = [
+  { id: 1, title: 'Title 1' },
+  { id: 2, title: 'Title 2' },
+]
 
 function App() {
-  const [count, setCount] = useState(0)
+  const queryClient = useQueryClient()
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Tanstack query</h1>
-    </>
-  )
+  const postsQuery = useQuery({
+    queryKey: ["posts"],
+    // queryFn: () => Promise.reject("Error message"),
+    queryFn: () => wait(1000).then(() => [...POSTS]),
+  })
+
+  const newPostMutation = useMutation({
+    mutationFn: title => {
+      return wait(1000).then(() => POSTS.push({ id: crypto.randomUUID(), title }))
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(["posts"])
+    }
+  })
+
+  if (postsQuery.isLoading) return <h1>Loading.....</h1>
+
+  if (postsQuery.isError) {
+    return <pre>{JSON.stringify(postsQuery.error)}</pre>
+  }
+
+  return <div>
+    {postsQuery.data?.map(post => (
+      <div key={post.id}>{post.title}</div>
+    ))}
+
+    <button disabled={newPostMutation.isPending} onClick={() => newPostMutation.mutate("New Post")}>Add New</button>
+  </div>
+};
+
+function wait(duration: number) {
+  return new Promise(resolve => setTimeout(resolve, duration))
 }
 
 export default App
